767877a83430ef9f46a2b526b202d0563eea5752
Andony Sanchez <52758052+andonysan@users.noreply.github.com>
2023-07-19 13:32:09 -0300

    Se agrego 2 rutas para el header y footer en app.js

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html
 app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname,'views','registro.html')));
 app.get('/carritoDeCompras',(req,res)=>res.sendFile(path.join(__dirname,'views','carritoDecompras.html')));
 app.get('/detalleDeProducto',(req,res)=>res.sendFile(path.join(__dirname,'views','detalleDeProducto.html')));
+// Extras
+app.get('/footer',(req,res)=>res.sendFile(path.join(__dirname,'views', 'partials','footer.html')));
+app.get('/header',(req,res)=>res.sendFile(path.join(__dirname,'views','partials','header.html')));
+// Error
 app.get('/*',(req,res)=>res.sendFile(path.join(__dirname,'views','404.html')));
+
 
 app.listen(PORT, ()=>console.log(`server running in http://localhost:${PORT}`));
