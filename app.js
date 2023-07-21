c15373671ebea601408d7ac3e5a19565422ddde2
Aldoleonel <aldoleonelchalup@gmail.com>
2023-07-20 22:32:53 -0300

    footer terminado

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = 3030;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/footer',(req,res)=>res.sendFile(path.join(__dirname,'views','partials','footer.html')));
 app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname,'views','registro.html')));
 app.get('/carritoDeCompras',(req,res)=>res.sendFile(path.join(__dirname,'views','carritoDecompras.html')));
