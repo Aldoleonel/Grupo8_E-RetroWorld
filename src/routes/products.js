90bedea98ccc93abc4950243b4a230a4a01c38f5
sebastian-tapia <sebastian.tapia35202@gmail.com>
2023-08-13 18:48:39 -0300

    [

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,5 +9,6 @@ router.get('/detail/:id?',productsController.detail);
 router.get('/cart/:id?',productsController.cart);
 
 router.get('/add',productsController.add);
-
+router.get('/edit',productsController.edit);
+router.get('/edit/:id?',productsController.edit)
 module.exports = router;
