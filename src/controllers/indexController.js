09b784171ec8693a5b078474c999fcb01d4a5ded
Axel <axelmichel515@gmail.com>
2023-08-26 03:04:39 -0300

    liste los productos en index y vista de productos

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 module.exports = {
     index : (req,res)=> {
-        return res.render('index')
+        return res.render('index',{
+			productsVisited : products.filter(product => product.estado === "visited"),
+			productsSale : products.filter(product => product.estado === "in-sale"),
+		    toThousand
+		});
     },
     admin : (req,res) => {
         return res.render('admin')
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {edit,add,detail} = require('../controllers/productsController');
+const {edit,add,detail, products} = require('../controllers/productsController');
 const router = express.Router();
 
 /*RUTAS PARA DETALLE_DE_PRODUCTO*/
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/detail/:id?',detail);
 router.get('/add',add);
 router.get('/edit',edit);
+router.get('/products',products)
 /* router.get('/edit/:id?',productsController.edit) */
 module.exports = router;
