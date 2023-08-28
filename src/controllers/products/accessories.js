9b59ac9486e1ba6469340f88567040bb902ee273
Axel <axelmichel515@gmail.com>
2023-08-28 06:16:36 -0300

    agregue vistas consolas y repuestos

---
+++ src/controllers/products/accessories.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+module.exports=(req,res)=>{
+    return res.render('accessories',{
+        products,
+        toThousand
+    });
+}
