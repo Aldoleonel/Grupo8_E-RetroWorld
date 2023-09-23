3a5fb6805050e46592a2e9844ac20e16f69bff2f
sebastian-tapia <sebastian.tapia35202@gmail.com>
2023-09-23 05:42:04 -0300

    se crea nueva vista de carta

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -1,18 +1,14 @@
 const Product = require("../../data/Product");
 const { readJSON,writeJson } = require("../../data/index");
 
-const products= readJSON('products')
+//const products= readJSON('products')
 
 module.exports = (req, res) => {
-
-
+    const products= readJSON('products')
     const newProduct = new Product(req.body)
-
-
     newProduct.image= req.file?req.file.filename:null
-        
-  
      products.push(newProduct)
     writeJson(products,'products')
-    return res.redirect('/')
+    
+    return res.redirect('/admin')
 }
