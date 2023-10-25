f7db425dcd000789894010d3d0c7f746d14f65f3
Aldoleonel <aldoleonelchalup@gmail.com>
2023-10-25 00:40:22 -0300

    vistas y registro finalizado

--- src/controllers/products/accessories.js
+++ src/controllers/products/accessories.js
@@ -4,13 +4,14 @@ const db = require('../../database/models')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports=(req,res)=>{
-    db.Product.findAll()
-        .then(products => {
-            console.log(products);
-            return res.render('accessories',{
-                        accessories: products.filter(product => product.type === "repuesto"),
-                        toThousand,
-                        
-            });
-        })
+    db.Product.findAll({
+        where: { typeId: '2' }
+    })
+    .then(consoles => {
+        console.log(consoles);
+        return res.render('consoles', {
+            consoles,
+            toThousand
+        });
+    })
 }
