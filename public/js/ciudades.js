97c6ce4cdb9815759cc685b428d54ca97e37b9c4
Aldoleonel <aldoleonelchalup@gmail.com>
2023-12-10 11:45:07 -0300

    errores reparados

--- public/js/ciudades.js
+++ public/js/ciudades.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const provinceSelect = document.getElementById('province');
     const citySelect = document.getElementById('city');
 
-    // Obtener lista de provincias desde la API al cargar la página
+    
     fetch('https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre')
         .then(response => response.json())
         .then(data => {
-            // Iterar sobre las provincias y agregarlas al selector
+            
             data.provincias.forEach(provincia => {
                 const option = document.createElement('option');
                 option.value = provincia.id;
@@ -16,18 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => console.error('Hubo un problema al obtener las provincias:', error));
 
-    // Evento al seleccionar una provincia para cargar las ciudades correspondientes
+    
     provinceSelect.addEventListener('change', () => {
         const selectedProvinceId = provinceSelect.value;
 
-        // Obtener lista de municipios de la provincia seleccionada desde la API
+        
         fetch(`https://apis.datos.gob.ar/georef/api/municipios?provincia=${selectedProvinceId}&campos=id,nombre&max=100`)
             .then(response => response.json())
             .then(data => {
-                // Limpiar select de ciudades
+                
                 citySelect.innerHTML = '';
 
-                // Iterar sobre los municipios y agregarlos al selector
+                
                 data.municipios.forEach(ciudad => {
                     const option = document.createElement('option');
                     option.value = ciudad.id;
